622c2b4c9174dc90113adedea804dfd1ff57d71f
Lennart Nicolas Krebs <contact@lennartnicolas.de>
2021-03-17 16:21:04 +0100

    Added unit circle implementation

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -3,27 +3,113 @@
 //==============================================================================
 MainComponent::MainComponent()
 {
-    setSize (600, 400);
+    addAndMakeVisible(circleSlider);
+    addAndMakeVisible(freqSlider);
+
+    circleSlider.setValue(5);
+    circleSlider.setRange(1, 100, 1);
+    circleSlider.setSliderStyle(juce::Slider::SliderStyle::LinearHorizontal);
+    circleSlider.setTextBoxStyle(juce::Slider::TextBoxLeft, true, 50, 20);
+    circleSlider.setSkewFactorFromMidPoint(15);
+    circleSlider.onValueChange = [this](){
+        updateCircleNumber((int)circleSlider.getValue());
+    };
+    
+    freqSlider.setValue(0.1);
+    freqSlider.setRange(0.01, 1, 0.01);
+    freqSlider.setSliderStyle(juce::Slider::SliderStyle::LinearHorizontal);
+    freqSlider.setTextBoxStyle(juce::Slider::TextBoxLeft, true, 50, 20);
+    freqSlider.setSkewFactorFromMidPoint(0.4);
+    freqSlider.onValueChange = [this](){
+        frequency = freqSlider.getValue();
+    };
+    
+    sampleRate = 30;
+    setFramesPerSecond(sampleRate);
+    
+    setSize (900, 500);
+    origin = juce::Point<float>((float)(getWidth()/4.f), (float)(getHeight()/2));
+    midPoint = juce::Point<float>((float)getWidth()/2,(float)getHeight()/2);
+    
+    radius = 75;
+    circleNumber = 10;
+    updateCircleNumber(circleNumber);
+
 }
 
 MainComponent::~MainComponent()
 {
 }
 
-//==============================================================================
+void MainComponent::update()
+{
+    x = origin.x;
+    y = origin.y;
+    
+    for(int i = 0; i < circleNumber; i++){
+        prevx = x;
+        prevy = y;
+        float n = i * 2 + 1;
+        angle = (float) getFrameCounter() / sampleRate * juce::MathConstants<float>::twoPi * frequency;
+        float r = radius * (4 / (n * juce::MathConstants<float>::pi));
+        x += r * std::cos(n * angle);
+        y += r * std::sin(n * angle);
+        circleAreas[i].setSize(r * 2, r * 2);
+        circleAreas[i].setCentre(prevx, prevy);
+        circleLines[i].setStart(prevx, prevy);
+        circleLines[i].setEnd(x, y);
+    }
+    
+    wave.push_front(y);
+    
+    if(wave.size() >= getWidth()/2) wave.pop_back();
+}
+
+//==============================================================================llllll
 void MainComponent::paint (juce::Graphics& g)
 {
-    // (Our component is opaque, so we must completely fill the background with a solid colour)
     g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
+    g.setColour(juce::Colours::white);
+    
+    g.setOpacity(0.3);
+    for(const auto& area : circleAreas){
 
-    g.setFont (juce::Font (16.0f));
-    g.setColour (juce::Colours::white);
-    g.drawText ("Hello World!", getLocalBounds(), juce::Justification::centred, true);
+        g.drawEllipse(area, 1.f);
+    }
+    
+    g.setOpacity(1.0);
+    for(const auto& line : circleLines)
+        g.drawLine(line);
+    
+    for(int i = 1; i < wave.size(); i++)
+        g.drawLine(midPoint.x + i - 1, wave[i - 1], midPoint.x + i, wave[i]);
+    
+    g.drawLine(x, y, midPoint.x, y);
+    
 }
 
 void MainComponent::resized()
 {
-    // This is called when the MainComponent is resized.
-    // If you add any child components, this is where you should
-    // update their positions.
+    circleSlider.setBounds(getWidth()/4 - 100, getHeight() - getHeight()/4, 200, 30);
+    freqSlider.setBounds(getWidth()/4 - 100, getHeight() - getHeight()/4 + 30, 200, 30);
+}
+
+juce::Rectangle<float> MainComponent::getAreaForCircle(float radius, const juce::Point<float>& position)
+{
+    juce::Rectangle<float> area;
+    area.setSize(radius * 2, radius * 2);
+    area.setCentre(position);
+    return area;
+}
+
+void MainComponent::updateCircleNumber(int cNumber)
+{
+    circleNumber = cNumber;
+    circleAreas.clear();
+    circleLines.clear();
+    
+    for(int i = 0; i < circleNumber; i++){
+        circleAreas.push_back(getAreaForCircle(0.0f, juce::Point<float> (0,0)));
+        circleLines.push_back(juce::Line<float>(0,0,0,0));
+    }
 }
--- Source/MainComponent.h
+++ Source/MainComponent.h
@@ -7,7 +7,7 @@
     This component lives inside our window, and this is where you should put all
     your controls and content.
 */
-class MainComponent  : public juce::Component
+class MainComponent  : public juce::AnimatedAppComponent
 {
 public:
     //==============================================================================
@@ -17,11 +17,31 @@ public:
     //==============================================================================
     void paint (juce::Graphics&) override;
     void resized() override;
+    void update() override;
+    juce::Rectangle<float> getAreaForCircle(float radius, const juce::Point<float>& position);
+    void updateCircleNumber(int cNumber);
 
 private:
     //==============================================================================
-    // Your private member variables go here...
+    juce::Point<float> origin;
+    juce::Point<float> midPoint;
 
+    std::vector<juce::Rectangle<float> > circleAreas;
+    std::vector<juce::Line<float> > circleLines;
+    int circleNumber;
+    
+    float x;
+    float y;
+    float prevx;
+    float prevy;
+    float angle;
+    float frequency;
+    int sampleRate;
+    
+    std::deque<float> wave;
+
+    juce::Slider circleSlider;
+    juce::Slider freqSlider;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainComponent)
 };
